5d7327ece9e812df383ce193d5e641c676cc0432
Yamila Moreno <yamila.ms@gmail.com>
2014-12-29 18:20:04 +0100

    Add jUnit testing

--- CalcTest.groovy
+++ CalcTest.groovy
@@ -1,28 +1,43 @@
-def myCalc
+class MyTestCase extends GroovyTestCase {
+    Calc myCalc
 
-// adition
-myCalc = new Calc()
-assert myCalc.add(2) == 2
-assert myCalc.add(2) != 1
+    void testAdition() {
+        myCalc = new Calc()
+        assert myCalc.add(2) == 2
+        assert myCalc.add(2) != 1
+    }
 
-// substraction
-myCalc = new Calc()
-assert myCalc.sub(3) == -3
+    void testSubstraction() {
+        myCalc = new Calc()
+        assert myCalc.sub(3) == -3
+    }
 
-// product
-myCalc = new Calc(res:1)
-assert myCalc.prod(3) == 3
+    void testProduct() {
+        myCalc = new Calc(res:1)
+        assert myCalc.prod(3) == 3
+    }
 
-// division
-myCalc = new Calc(res:10)
-assert myCalc.div(5) == 2
+    void testDivision() {
+        myCalc = new Calc(res:10)
+        assert myCalc.div(5) == 2
+    }
 
-// combine operations
-myCalc = new Calc()
-assert myCalc.add(2) == 2
-assert myCalc.add(2) == 4
-assert myCalc.sub(1) == 3
-assert myCalc.sub(1) == 2
-assert myCalc.prod(2) == 4
-assert myCalc.prod(3) == 12
-assert myCalc.div(2) == 6
+    void testCombine() {
+        myCalc = new Calc()
+        assert myCalc.add(2) == 2
+        assert myCalc.add(2) == 4
+        assert myCalc.sub(1) == 3
+        assert myCalc.sub(1) == 2
+        assert myCalc.prod(2) == 4
+        assert myCalc.prod(3) == 12
+        assert myCalc.div(2) == 6
+    }
+
+    void testError() {
+        myCalc = new Calc(res:1)
+        shouldFail {
+            myCalc.prod("a")
+        }
+    }
+
+}

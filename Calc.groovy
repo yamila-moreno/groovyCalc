90278095b24fff95ad965e3f8a5aacfdb8782d57
Yamila Moreno <yamila.ms@gmail.com>
2014-12-29 12:47:49 +0100

    Add main groovy files

---
+++ Calc.groovy
@@ -0,0 +1,24 @@
+class Calc {
+    int res = 0
+
+    def add(n) {
+        res = res + n
+        return res
+    }
+
+    def sub(n) {
+        res = res - n
+        return res
+    }
+
+    def prod(n) {
+        res = res * n
+        return res
+    }
+
+    def div(n) {
+        res = res / n
+        return res
+    }
+
+}
---
+++ test.groovy
@@ -0,0 +1,28 @@
+def myCalc
+
+// adition
+myCalc = new Calc()
+assert myCalc.add(2) == 2
+assert myCalc.add(2) != 1
+
+// substraction
+myCalc = new Calc()
+assert myCalc.sub(3) == -3
+
+// product
+myCalc = new Calc(res:1)
+assert myCalc.prod(3) == 3
+
+// division
+myCalc = new Calc(res:10)
+assert myCalc.div(5) == 2
+
+// combine operations
+myCalc = new Calc()
+assert myCalc.add(2) == 2
+assert myCalc.add(2) == 4
+assert myCalc.sub(1) == 3
+assert myCalc.sub(1) == 2
+assert myCalc.prod(2) == 4
+assert myCalc.prod(3) == 12
+assert myCalc.div(2) == 6
